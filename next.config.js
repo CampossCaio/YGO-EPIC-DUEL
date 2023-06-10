3a90e1740094b6a97b99f34f354747e7c917d5ff
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2023-06-10 14:13:22 -0300

    chore: the game screenshots had been added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
-
 /** @type {import('next').NextConfig} */
 
-module.exports = withPWA({
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-});
+};
